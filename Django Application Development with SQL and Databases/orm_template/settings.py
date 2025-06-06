78bb213f72f053d3e864da091bf7695675047d47
BogdanBargaoanu <bn19bba@yahoo.com>
2025-06-06 12:54:18 +0300

    orm_template

---
+++ Django Application Development with SQL and Databases/orm_template/settings.py
@@ -0,0 +1,15 @@
+# PostgreSQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'I6aWAXLDebu0k1fei26NkgCp',
+        'HOST': 'postgres',
+        'PORT': '5432',
+    }
+}
+INSTALLED_APPS = (
+    'standalone',
+)
+SECRET_KEY = 'SECRET KEY for this Django Project'
